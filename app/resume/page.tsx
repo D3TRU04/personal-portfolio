a6b990568cf13579f200edec334519a05f8f79b9
D3TRU04 <danieltruong1007@gmail.com>
2026-03-21 23:31:44 -0500

    Replacing old resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -34,7 +34,7 @@ const ResumePage = () => {
                     <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
                   </svg>
                   <div>
-                    <p className="font-medium">Dan_Truong_Resume_2027.pdf</p>
+                    <p className="font-medium">DanTruongResume2027.pdf</p>
                     <p className="text-sm text-secondary">PDF Document • 117 KB</p>
                   </div>
                 </div>
